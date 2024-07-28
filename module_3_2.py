f04154bbd18d7ee40963e8d7e0f4afed15ae8735
KravchenkoII <group777@mail.ru>
2024-07-28 17:42:13 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,7 +1,24 @@
-def send_email(message=1, recipient="we", sender = "university.help@gmail.com"):
-    if (recipient.find('@') or sender.find('@')) !=-1:
-        print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
-    else:
-        print('Good')
-    print(recipient.find('@') != -1)
-print(send_email())
+val1 = ('.com', 'ru', '.net')
+val2 = ('.uk')
+
+
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if '@' in recipient or sender:
+        if recipient.endswith(val1):
+            if sender.endswith(val2):
+                return (f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+            if recipient.lower() == sender.lower():
+                return ("Нельзя отправить письмо самому себе!")
+            else:
+                if sender == 'urban.info@gmail.com':
+                    return (f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+                else:
+                    return (f'Письмо успешно отправлено с адреса {sender}> на адрес {recipient}')
+
+
+print(send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com'))
+print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', \
+                 sender='urban.info@gmail.com'))
+print(send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', \
+                 sender='urban.teacher@mail.uk'))
+print(send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
