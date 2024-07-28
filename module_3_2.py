160a8b4d25f4ef1c6b735d7e9bf710faed5e4d45
KravchenkoII <group777@mail.ru>
2024-07-28 17:43:52 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -17,8 +17,6 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
 
 
 print(send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com'))
-print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', \
-                 sender='urban.info@gmail.com'))
-print(send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', \
-                 sender='urban.teacher@mail.uk'))
+print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com'))
+print(send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk'))
 print(send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
